0412d32a190a2cc726f8e462822c67cd65cf2ec2
unknown <waldteufel@ukr.net>
2020-01-03 23:36:33 +0200

    qqq

---
+++ eclipse/assets/js/version-control.js
@@ -0,0 +1,1 @@
+"use strict";var v={animateCss:"3.5.2",materialFontCss:1,slickCss:"1.8.1",slickThemeCss:"1.8.1",countriesJs:1,jqueryJs:"3.3.1",slickJs:"1.8.1",parallaxJs:"1.5.0",wowJs:"1.1.3",indexCss:1,pagesCss:1,modulesCss:1,mainJs:1,indexJs:1,modulesJs:1,companyInfoJs:1};
