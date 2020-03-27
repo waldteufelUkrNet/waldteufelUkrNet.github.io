ec19f55fa91bb30a91321cc8eec7de3786d59d93
unknown <waldteufel@ukr.net>
2020-03-27 13:01:32 +0200

    qqq

--- myBooks/js/modules.js
+++ myBooks/js/modules.js
@@ -823,36 +823,30 @@ function setFont() {
 }
 /* ↑↑↑ /FUNCTIONS DECLARATION ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
-"use strict"; // consol module
-////////////////////////////////////////////////////////////////////////////////
-
-/* ↓↓↓ ??? ↓↓↓ */
-
-var isConsolOpen = false;
-
-document.getElementById('consol-button').onclick = function () {
-  if (isConsolOpen) {
-    document.getElementById('consol').style.height = '0px';
-  } else {
-    document.getElementById('consol').style.height = '50vh';
-  }
-
-  isConsolOpen = !isConsolOpen;
-};
-
-document.getElementById('ls-button').onclick = function () {
-  localStorage.clear();
-  conlog('localStorage: ' + JSON.stringify(localStorage));
-};
-
-function conlog(value) {
-  var p = '<p>' + value + '</p>';
-  document.getElementById('consol').insertAdjacentHTML('beforeEnd', p);
-}
-
-;
-/* ↑↑↑ /??? ↑↑↑ */
-////////////////////////////////////////////////////////////////////////////////
+// "use strict";
+// // consol module
+// ////////////////////////////////////////////////////////////////////////////////
+// /* ↓↓↓ ??? ↓↓↓ */
+//   let isConsolOpen = false;
+//   document.getElementById('consol-button').onclick = function() {
+//     if (isConsolOpen) {
+//       document.getElementById('consol').style.height = '0px';
+//     } else {
+//       document.getElementById('consol').style.height = '50vh';
+//     }
+//      isConsolOpen = !isConsolOpen;
+//   };
+//   document.getElementById('ls-button').onclick = function() {
+//     localStorage.clear();
+//     conlog('localStorage: ' + JSON.stringify(localStorage));
+//   };
+//   function conlog (value) {
+//     let p = '<p>' + value + '</p>';
+//     document.getElementById('consol').insertAdjacentHTML('beforeEnd',p);
+//   };
+// /* ↑↑↑ /??? ↑↑↑ */
+// ////////////////////////////////////////////////////////////////////////////////
+"use strict";
 "use strict"; // top-book-panel module
 ////////////////////////////////////////////////////////////////////////////////
 
--- myBooks/modules/consol/consol.js
+++ myBooks/modules/consol/consol.js
@@ -1,25 +1,25 @@
-"use strict";
-// consol module
-////////////////////////////////////////////////////////////////////////////////
-/* ↓↓↓ ??? ↓↓↓ */
-  let isConsolOpen = false;
-  document.getElementById('consol-button').onclick = function() {
-    if (isConsolOpen) {
-      document.getElementById('consol').style.height = '0px';
-    } else {
-      document.getElementById('consol').style.height = '50vh';
-    }
-     isConsolOpen = !isConsolOpen;
-  };
+// "use strict";
+// // consol module
+// ////////////////////////////////////////////////////////////////////////////////
+// /* ↓↓↓ ??? ↓↓↓ */
+//   let isConsolOpen = false;
+//   document.getElementById('consol-button').onclick = function() {
+//     if (isConsolOpen) {
+//       document.getElementById('consol').style.height = '0px';
+//     } else {
+//       document.getElementById('consol').style.height = '50vh';
+//     }
+//      isConsolOpen = !isConsolOpen;
+//   };
 
-  document.getElementById('ls-button').onclick = function() {
-    localStorage.clear();
-    conlog('localStorage: ' + JSON.stringify(localStorage));
-  };
+//   document.getElementById('ls-button').onclick = function() {
+//     localStorage.clear();
+//     conlog('localStorage: ' + JSON.stringify(localStorage));
+//   };
 
-  function conlog (value) {
-    let p = '<p>' + value + '</p>';
-    document.getElementById('consol').insertAdjacentHTML('beforeEnd',p);
-  };
-/* ↑↑↑ /??? ↑↑↑ */
-////////////////////////////////////////////////////////////////////////////////
+//   function conlog (value) {
+//     let p = '<p>' + value + '</p>';
+//     document.getElementById('consol').insertAdjacentHTML('beforeEnd',p);
+//   };
+// /* ↑↑↑ /??? ↑↑↑ */
+// ////////////////////////////////////////////////////////////////////////////////
