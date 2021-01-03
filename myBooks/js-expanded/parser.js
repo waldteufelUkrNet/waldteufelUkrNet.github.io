f3be5a4816fde978f81c56c243d37454b261ae53
waldteufelUkrNet <waldteufel@ukr.net>
2021-01-03 23:24:23 +0200

    q

---
+++ myBooks/js-expanded/parser.js
@@ -0,0 +1,32 @@
+"use strict";
+// dbase.js
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ VARIABLES DECLARATION ↓↓↓ */
+  // let keyForCompare    = 'author',
+  //     bookList         = document.querySelector('.books-list'),
+  //     ls               = localStorage,
+  //     isSerchFieldOpen = false,
+  //     bookListType;
+/* ↑↑↑ /VARIABLES DECLARATION ↑↑↑ */
+
+/* ↓↓↓ MAIN LOGIC ↓↓↓ */
+
+/* ↑↑↑ /MAIN LOGIC ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
+/* ↓↓↓ FUNCTIONS DECLARATION ↓↓↓ */
+  /**
+   * здійснює лексикографічне сортування масиву об'єктів phoneBook за ключем,
+   * записаним в змінній keyForCompare
+   * @param  {object} a об'єкт, елемент масиву
+   * @param  {object} b об'єкт, елемент масиву
+   * @return {[number]} результат порівняння
+   */
+  // function compare( a, b ) {
+  //   return a[keyForCompare].localeCompare(b[keyForCompare]);
+  // }
+  
+  function cleanTextArea(id) {
+    document.querySelector('#' + id).value = '';
+  }
+/* ↑↑↑ /FUNCTIONS DECLARATION ↑↑↑ */
+////////////////////////////////////////////////////////////////////////////////
