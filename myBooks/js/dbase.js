36909957def75ef44200cad91ccdd13dac9598e3
waldteufelUkrNet <waldteufel@ukr.net>
2021-01-24 23:42:18 +0200

    q

--- myBooks/js/dbase.js
+++ myBooks/js/dbase.js
@@ -11,244 +11,304 @@
     },
     {
       id     : '00001',
-      author : 'Макаров Н.А., Антонов Г.В.',
-      name   : 'Психологическая самоподкоговка к рукопашному бою',
-      genre  : 'Психологія'
+      author : 'Irina Weber',
+      name   : 'Lustige Naturwissenschaft', // 42
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00002',
-      author : 'Тарас А.Е.',
-      name   : 'Психологическая подготовка бойца',
-      genre  : 'Психологія'
+      author : 'Hanns Heinz Ewers',
+      name   : 'Die Spinne', // 46
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00003',
-      author : 'Adolf Hitler',
-      name   : 'Mein Kampf',
-      genre  : 'genre'
+      author : 'Theodor Storm',
+      name   : 'Immensee', // 55
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00004',
-      author : 'Сашко А.',
-      name   : 'Боль',
-      genre  : 'Психологія'
+      author : 'Theodor Storm',
+      name   : 'Die Regentrude', // 57
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00005',
-      author : 'Йооп Сгрийверс',
-      name   : 'Как быть крысой. Искусство интриг и выживания на работе',
-      genre  : 'Психологія'
+      author : 'Joseph Roth',
+      name   : 'Die Legende vom heiligen Trinker', // 61
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00006',
-      author : 'Ігнатій Лойола',
-      name   : 'Духовні вправи',
-      genre  : 'Психологія'
+      author : 'E.T.A. Hoffmann',
+      name   : 'Der Sandmann', // 71
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00007',
-      author : 'Irina Weber',
-      name   : 'Lustige Naturwissenschaft',
+      author : 'Klaus-Peter Wolf',
+      name   : 'Total wahre Flunkergeschichten. Erzählt von einem rotzfrechen Mädchen', // 77
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00008',
-      author : 'Ernest Hemingway',
-      name   : 'The Killers',
-      genre  : 'метод Франка: англ.мова'
+      author : 'Wilhelm Hauff',
+      name   : 'Das kalte Herz', // 79
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00009',
-      author : 'Іваничук Роман',
-      name   : 'Орда',
-      genre  : 'Укр.література'
+      author : 'noname',
+      name   : 'Dies Leben kömmt mir vor als eine Renne-Bahn. Deutsche Gedichte aus dem 17. Jahrhundert.', // 80
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00010',
-      author : 'Klaus-Peter Wolf',
-      name   : 'Total wahre Flunkergeschichten. Erzählt von einem rotzfrechen Mädchen',
+      author : 'Stefan Zweig',
+      name   : 'Schachnovelle', //89
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00011',
-      author : 'Ray Bradbury',
-      name   : 'A Sound of Thunder',
-      genre  : 'метод Франка: англ.мова'
+      author : 'Jacob und Wilhelm Grimm',
+      name   : 'Irische Elfenmärchen', // 92
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00012',
-      author : 'Washington Irving',
-      name   : 'Rip Van Winkle',
-      genre  : 'метод Франка: англ.мова'
+      author : 'Otfried Preussler',
+      name   : 'Die kleine Hexe', // 94
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00013',
-      author : 'Francis Scott Fitzgerald',
-      name   : 'The diamond as big as the Ritz',
-      genre  : 'метод Франка: англ.мова'
+      author : 'Stefan Zweig',
+      name   : 'Brief einer Unbekannten', // 96
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00014',
-      author : 'Otfried Preussler',
-      name   : 'Die kleine Hexe',
+      author : 'Adelbert von Chamisso',
+      name   : 'Peter Schlemihls wundersame Geschichte', // 104
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00015',
-      author : 'author',
-      name   : 'Англійська фонетика',
-      genre  : 'genre'
+      author : 'Wilhelm Hauff',
+      name   : 'Kalif Storch. Der kleine Muck. Der Zwerg Nase', // 109
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00016',
-      author : 'Stefan Zweig',
-      name   : 'Schachnovelle',
+      author : 'Franz Kafka',
+      name   : 'Die Verwandlung', // 112
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00017',
-      author : 'Форсайт Фредерик',
-      name   : 'День Шакала',
-      genre  : 'політичний детектив'
+      author : 'Heinrich Böll',
+      name   : 'Das Brot der frühen Jahre', // 113
+      genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00018',
-      author : 'Heinrich Böll',
-      name   : 'Das Brot der frühen Jahre',
+      author : 'Wolfgang Borchert',
+      name   : 'Draußen vor der Tür', // 121
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00019',
-      author : 'Friedrich Durrenmatt',
-      name   : 'Der Richter und sein Henker',
+      author : 'Ludwig Thoma',
+      name   : 'Lausbubengeschichten', // 129
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00020',
-      author : 'Erich Kästner',
-      name   : 'Das doppelte Lottchen',
+      author : 'Ludwig Thoma',
+      name   : 'Tante Frieda', // 143
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00021',
-      author : 'Erich Kästner',
-      name   : 'Drei Männer im Schnee',
+      author : 'Friedrich Durrenmatt',
+      name   : 'Der Richter und sein Henker', // 141
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00022',
-      author : 'Hansjorg Martin',
-      name   : 'Meine schone Morderin',
+      author : 'Arthur Schnitzler',
+      name   : 'Traumnovelle', // 150
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00023',
-      author : 'Annemarie Selinko',
-      name   : 'Heute heiratet mein Mann',
+      author : 'Erich Kästner',
+      name   : 'Das doppelte Lottchen', // 154
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00024',
-      author : 'E. M. Remarque',
-      name   : 'Drei Kameraden',
+      author : 'Jacob und Wilhelm Grimm',
+      name   : 'Grimms Märchen', // 154
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00025',
-      author : 'Hanns Heinz Ewers',
-      name   : 'Die Spinne',
+      author : 'Irmgard Keun',
+      name   : 'Das Mädchen, mit dem die Kinder nicht verkehren durften', // 154
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00026',
-      author : 'Joseph Roth',
-      name   : 'Die Legende vom heiligen Trinker',
+      author : 'Hans Fallada',
+      name   : 'Zwei zarte Lämmchen weiß wie Schnee', // 162
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00027',
-      author : 'Wilhelm Hauff',
-      name   : 'Das kalte Herz',
+      author : 'noname',
+      name   : 'Der liebe Augustin (Sagen aus Wien)', // 165
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00028',
-      author : 'Franz Kafka',
-      name   : 'Die Verwandlung',
+      author : 'Erich Kästner',
+      name   : 'Emil und die Detektive', // 171
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00029',
-      author : 'Theodor Storm',
-      name   : 'Die Regentrude',
+      author : 'Friedrich Glauser',
+      name   : 'Beichte in der Nacht. Erzählungen', // 175
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00030',
-      author : 'Theodor Storm',
-      name   : 'Immensee',
+      author : 'Gerhard Holtz-Baumert',
+      name   : 'Alfons Zitterbacke. Geschichten eines Pechvogels', // 199
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00031',
-      author : 'Hans Fallada',
-      name   : 'Zwei zarte Lämmchen weiß wie Schnee',
+      author : 'Joseph Roth',
+      name   : 'Hotel Savoy', // 201
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00032',
-      author : 'Stefan Zweig',
-      name   : 'Brief einer Unbekannten',
+      author : 'Erich Kästner',
+      name   : 'Drei Männer im Schnee', // 261
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00033',
-      author : 'Ludwig Thoma',
-      name   : 'Lausbubengeschichten',
+      author : 'Annemarie Selinko',
+      name   : 'Heute heiratet mein Mann', // 265
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00034',
-      author : 'Erich Kästner',
-      name   : 'Emil und die Detektive',
+      author : 'Hansjorg Martin',
+      name   : 'Meine schone Morderin', // 271
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00035',
-      author : 'Otfried Preußler',
+      author : 'Otfried Preußler', // 273
       name   : 'Krabat',
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00036',
-      author : 'E.T.A. Hoffmann',
-      name   : 'Der Sandmann',
+      author : 'noname',
+      name   : 'Wandrers Nachtlied', // 298
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00037',
-      author : 'Jacob und Wilhelm Grimm',
-      name   : 'Irische Elfenmärchen',
+      author : 'E. M. Remarque',
+      name   : 'Drei Kameraden', // 409
       genre  : 'метод Франка: нім.мова'
     },
     {
       id     : '00038',
-      author : 'Ludwig Thoma',
-      name   : 'Tante Frieda',
-      genre  : 'метод Франка: нім.мова'
+      author : 'Макаров Н.А., Антонов Г.В.',
+      name   : 'Психологическая самоподкоговка к рукопашному бою',
+      genre  : 'Психологія'
     },
     {
       id     : '00039',
-      author : 'Adelbert von Chamisso',
-      name   : 'Peter Schlemihls wundersame Geschichte',
-      genre  : 'метод Франка: нім.мова'
+      author : 'Тарас А.Е.',
+      name   : 'Психологическая подготовка бойца',
+      genre  : 'Психологія'
     },
     {
       id     : '00040',
-      author : 'Irmgard Keun',
-      name   : 'Das Mädchen, mit dem die Kinder nicht verkehren durften',
-      genre  : 'метод Франка: нім.мова'
-    }
+      author : 'Сашко А.',
+      name   : 'Боль',
+      genre  : 'Психологія'
+    },
+    {
+      id     : '00041',
+      author : 'Adolf Hitler',
+      name   : 'Mein Kampf',
+      genre  : 'ідеологія'
+    },
+    {
+      id     : '00042',
+      author : 'Йооп Сгрийверс',
+      name   : 'Как быть крысой. Искусство интриг и выживания на работе',
+      genre  : 'Психологія'
+    },
+    {
+      id     : '00043',
+      author : 'Ігнатій Лойола',
+      name   : 'Духовні вправи',
+      genre  : 'Психологія'
+    },
+    {
+      id     : '00044',
+      author : 'Іваничук Роман',
+      name   : 'Орда',
+      genre  : 'Укр.література'
+    },
+    {
+      id     : '00045',
+      author : 'Форсайт Фредерик',
+      name   : 'День Шакала',
+      genre  : 'політичний детектив'
+    },
+    {
+      id     : '00046',
+      author : 'waldteufel',
+      name   : 'Англійська фонетика',
+      genre  : 'genre'
+    },
+    {
+      id     : '00047',
+      author : 'Ernest Hemingway',
+      name   : 'The Killers',
+      genre  : 'метод Франка: англ.мова'
+    },
+    {
+      id     : '00048',
+      author : 'Ray Bradbury',
+      name   : 'A Sound of Thunder',
+      genre  : 'метод Франка: англ.мова'
+    },
+    {
+      id     : '00049',
+      author : 'Washington Irving',
+      name   : 'Rip Van Winkle',
+      genre  : 'метод Франка: англ.мова'
+    },
+    {
+      id     : '00050',
+      author : 'Francis Scott Fitzgerald',
+      name   : 'The diamond as big as the Ritz',
+      genre  : 'метод Франка: англ.мова'
+    },
   ];
 /* ↑↑↑ /DATA BASE ↑↑↑ */
 ////////////////////////////////////////////////////////////////////////////////
